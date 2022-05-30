44f6dd6a97ae07a153c120237e2d5ee93640d18d
dulice <duliceellen600@gmail.com>
2022-05-30 18:44:19 +0630

    adjust search result

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,13 +1,10 @@
 import React, {useState, useEffect} from 'react'
 import "bootstrap/dist/css/bootstrap.min.css"
-import styled from "styled-components";
-import { BsFillPlayCircleFill} from "react-icons/bs";
-import { FaThumbsUp } from "react-icons/fa";
 import { useParams } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
 
 const TopRated = () => {
     const params = useParams();
-    const imagePath = "https://image.tmdb.org/t/p/w342/";
     const [TopRated, setTopRated] = useState([]);
     const fetchTopRated = async (name) => {
         const res = await fetch(`https://api.themoviedb.org/3/search/multi?api_key=${process.env.REACT_APP_API_KEY}&query=${name}`);
@@ -25,23 +22,7 @@ const TopRated = () => {
             <div className="row">
                 {TopRated.map((movie) => {
                     return (
-                        <MCard className="col-12 col-md-3 mb-4" key={movie.id}>
-                            <div className="card rounded shadow">
-                                <div className="card-body position-relative">
-                                    <span>
-                                        <BsFillPlayCircleFill/>
-                                    </span>
-                                    {movie.poster_path? <img className="w-100 rounded" src={`${imagePath}${movie.poster_path}`} alt={movie.title} /> : (<div className="movie-cover"><p >movie image unavalible</p></div>)}
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <h6 >{movie.name}{movie.title}</h6>
-                                        <div className="d-flex align-items-center">
-                                        <FaThumbsUp/>
-                                        <h6 className="text-black-50 m-0 ps-1">{movie.vote_average}</h6>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </MCard>
+                        <MovieCard movie={movie} key={movie.id}/>
                     )
                 })}
             </div>
@@ -50,24 +31,5 @@ const TopRated = () => {
     </div>
   )
 }
-
-const MCard = styled.div`
-  transition: .5s;
-  span {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    font-size: 2rem;
-    display: none;
-  }
-  :hover {
-    transform: translateY(-5px) scale(1.05);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-    span {
-      display: block;
-    }
-  }
-`
 
 export default TopRated
